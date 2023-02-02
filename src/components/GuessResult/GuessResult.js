9c67df502b31868ee7915922ad299e53fb07f83e
rizalramadan <rizalramadan.dev@gmail.com>
2023-02-02 19:30:45 +0700

    Exercise 3: Guess slot. Done!

--- src/components/GuessResult/GuessResult.js
+++ src/components/GuessResult/GuessResult.js
@@ -1,14 +1,13 @@
 import React from "react";
+import Guess from "../Guess";
+import { range } from "../../utils";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 function GuessResult({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map((item, index) => {
-        return (
-          <p className="guess" key={index}>
-            {item}
-          </p>
-        );
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => {
+        return <Guess value={guesses[num]} key={num} />;
       })}
     </div>
   );
